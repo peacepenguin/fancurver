5d271f79e4b8f7f6f91d487fedd1387c94873185
Ben <peacepenguin@users.noreply.github.com>
2021-11-17 10:46:57 -0700

    remove tray
    tray icon never worked, remove it

--- fancurver/Program.cs
+++ fancurver/Program.cs
@@ -6,10 +6,6 @@ using System;
 using System.Threading;
 using LibreHardwareMonitor.Hardware;
 
-//for tray icon:
-using System.Drawing;
-using System.Windows.Forms;
-
 
 //
 
@@ -366,26 +362,6 @@ namespace fancurver
             Console.WriteLine("Exiting now");
             Environment.Exit(0);
         }
-
-        public void CreateTextIcon(string str)
-        {
-            Font fontToUse = new Font("Microsoft Sans Serif", 16, FontStyle.Regular, GraphicsUnit.Pixel);
-            Brush brushToUse = new SolidBrush(Color.White);
-            Bitmap bitmapText = new Bitmap(16, 16);
-            Graphics g = System.Drawing.Graphics.FromImage(bitmapText);
-
-            IntPtr hIcon;
-
-            g.Clear(Color.Transparent);
-            g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SingleBitPerPixelGridFit;
-            g.DrawString(str, fontToUse, brushToUse, -4, -2);
-            hIcon = (bitmapText.GetHicon());
-
-            NotifyIcon notifyx = new NotifyIcon();
-            notifyx.Icon = System.Drawing.Icon.FromHandle(hIcon);
-            DestroyIcon(hIcon.ToInt32);
-        }
-
 
     }
 
